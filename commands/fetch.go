6bb3a383f21aa7e68ad0ece653310b4461593340
spf13 <steve.francia@gmail.com>
2014-07-19 02:36:32 -0400

    Storing Feeds in the Database

--- commands/fetch.go
+++ commands/fetch.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strings"
 	"time"
 
 	rss "github.com/jteeuwen/go-pkg-rss"
@@ -73,8 +74,141 @@ func PollFeed(uri string) {
 
 func chanHandler(feed *rss.Feed, newchannels []*rss.Channel) {
 	fmt.Printf("%d new channel(s) in %s\n", len(newchannels), feed.Url)
+	for _, ch := range newchannels {
+		chnl := chnlify(ch)
+		if err := Channels().Insert(chnl); err != nil {
+			if !strings.Contains(err.Error(), "E11000") {
+				fmt.Printf("Database error. Err: %v", err)
+			}
+		}
+	}
 }
 
 func itemHandler(feed *rss.Feed, ch *rss.Channel, newitems []*rss.Item) {
 	fmt.Printf("%d new item(s) in %s\n", len(newitems), feed.Url)
+	for _, item := range newitems {
+		itm := itmify(item, ch)
+		if err := Items().Insert(itm); err != nil {
+			if !strings.Contains(err.Error(), "E11000") {
+				fmt.Printf("Database error. Err: %v", err)
+			}
+		}
+	}
+}
+
+type Itm struct {
+	Date         time.Time
+	Key          string
+	ChannelKey   string
+	Title        string
+	FullContent  string
+	Links        []*rss.Link
+	Description  string
+	Author       rss.Author
+	Categories   []*rss.Category
+	Comments     string
+	Enclosures   []*rss.Enclosure
+	Guid         *string `bson:",omitempty"`
+	Source       *rss.Source
+	PubDate      string
+	Id           string `bson:",omitempty"`
+	Generator    *rss.Generator
+	Contributors []string
+	Content      *rss.Content
+	Extensions   map[string]map[string][]rss.Extension
+}
+
+type Chnl struct {
+	Key            string
+	Title          string
+	Links          []rss.Link
+	Description    string
+	Language       string
+	Copyright      string
+	ManagingEditor string
+	WebMaster      string
+	PubDate        string
+	LastBuildDate  string
+	Docs           string
+	Categories     []*rss.Category
+	Generator      rss.Generator
+	TTL            int
+	Rating         string
+	SkipHours      []int
+	SkipDays       []int
+	Image          rss.Image
+	ItemKeys       []string
+	Cloud          rss.Cloud
+	TextInput      rss.Input
+	Extensions     map[string]map[string][]rss.Extension
+	Id             string
+	Rights         string
+	Author         rss.Author
+	SubTitle       rss.SubTitle
+}
+
+func itmify(o *rss.Item, ch *rss.Channel) Itm {
+	var x Itm
+	x.Title = o.Title
+	x.Links = o.Links
+	x.ChannelKey = ch.Key()
+	x.Description = o.Description
+	x.Author = o.Author
+	x.Categories = o.Categories
+	x.Comments = o.Comments
+	x.Enclosures = o.Enclosures
+	x.Guid = o.Guid
+	x.PubDate = o.PubDate
+	x.Id = o.Id
+	x.Key = o.Key()
+	x.Generator = o.Generator
+	x.Contributors = o.Contributors
+	x.Content = o.Content
+	x.Extensions = o.Extensions
+	x.Date, _ = o.ParsedPubDate()
+
+	if o.Content != nil && o.Content.Text != "" {
+		x.FullContent = o.Content.Text
+	} else {
+		x.FullContent = o.Description
+	}
+
+	return x
+}
+
+func chnlify(o *rss.Channel) Chnl {
+	var x Chnl
+	x.Key = o.Key()
+	x.Title = o.Title
+	x.Links = o.Links
+	x.Description = o.Description
+	x.Language = o.Language
+	x.Copyright = o.Copyright
+	x.ManagingEditor = o.ManagingEditor
+	x.WebMaster = o.WebMaster
+	x.PubDate = o.PubDate
+	x.LastBuildDate = o.LastBuildDate
+	x.Docs = o.Docs
+	x.Categories = o.Categories
+	x.Generator = o.Generator
+	x.TTL = o.TTL
+	x.Rating = o.Rating
+	x.SkipHours = o.SkipHours
+	x.SkipDays = o.SkipDays
+	x.Image = o.Image
+	x.Cloud = o.Cloud
+	x.TextInput = o.TextInput
+	x.Extensions = o.Extensions
+	x.Id = o.Id
+	x.Rights = o.Rights
+	x.Author = o.Author
+	x.SubTitle = o.SubTitle
+
+	var keys []string
+	for _, y := range o.Items {
+		keys = append(keys, y.Key())
+	}
+	x.ItemKeys = keys
+
+	return x
 }
---
+++ commands/mongodb.go
@@ -0,0 +1,75 @@
+// Copyright © 2014 Steve Francia <spf@spf13.com>.
+//
+// Use of this source code is governed by an Apache2
+// license that can be found in the LICENSE file.
+
+package commands
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/spf13/viper"
+	"gopkg.in/mgo.v2"
+)
+
+var mongodbSession *mgo.Session
+
+func init() {
+	RootCmd.PersistentFlags().String("mongodb_uri", "mongodb://localhost:27017/", "Uri to connect to mongoDB")
+	viper.BindPFlag("mongodb_uri", RootCmd.PersistentFlags().Lookup("mongodb_uri"))
+	CreateUniqueIndexes()
+}
+
+func DBSession() *mgo.Session {
+	if mongodbSession == nil {
+		uri := os.Getenv("MONGODB_URI")
+		if uri == "" {
+			uri = viper.GetString("mongodb_uri")
+
+			if uri == "" {
+				log.Fatalln("No connection uri for MongoDB provided")
+			}
+		}
+
+		var err error
+		mongodbSession, err = mgo.Dial(uri)
+		if mongodbSession == nil || err != nil {
+			log.Fatalf("Can't connect to mongo, go error %v\n", err)
+		}
+
+		mongodbSession.SetSafe(&mgo.Safe{})
+	}
+	return mongodbSession
+}
+
+func Items() *mgo.Collection {
+	return DB().C("items")
+}
+
+func Channels() *mgo.Collection {
+	return DB().C("channels")
+}
+
+func DB() *mgo.Database {
+	return DBSession().DB(viper.GetString("dbname"))
+}
+
+func CreateUniqueIndexes() {
+	idx := mgo.Index{
+		Key:        []string{"key"},
+		Unique:     true,
+		DropDups:   true,
+		Background: true,
+		Sparse:     true,
+	}
+
+	if err := Items().EnsureIndex(idx); err != nil {
+		fmt.Println(err)
+	}
+
+	if err := Channels().EnsureIndex(idx); err != nil {
+		fmt.Println(err)
+	}
+}
